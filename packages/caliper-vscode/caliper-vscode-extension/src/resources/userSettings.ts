cb87e683d78774b97072dfdbbb62473c72c0e4b9
eravatee <eraraje1997@gmail.com>
2022-07-05 23:54:49 -0600

    Initial setup for vscode extension

---
+++ packages/caliper-vscode/caliper-vscode-extension/src/resources/userSettings.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { GeneralObject } from "./util";
+export const userSettings = [
+    {
+        "yaml.schemas": {
+            "https://github.com/eravatee/caliper/blob/packages/caliper-vscode/artifacts/default.json?raw=true": "*.yaml"
+        }
+    }
+] as GeneralObject[];
+
